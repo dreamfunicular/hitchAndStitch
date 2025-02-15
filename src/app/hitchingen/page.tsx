51b65c8374f99687ef6872bf5d2eb4477afd70c5
UnrelatedString <unrelatedstring@gmail.com>
2025-02-15 16:25:18 -0500

    things and stuff

--- src/app/hitchingen/page.tsx
+++ src/app/hitchingen/page.tsx
@@ -1,16 +1,40 @@
 'use client';
 
-function SeedField() {
+import { useState } from "react";
+
+function SeedTester() {
+
+  const [text, setText] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     e.stopPropagation();
-    console.log(e.target.value);
+    setText(e.target.value);
   }
 
   return (
-    <span>
-      Seed: <input type="text" onChange={handleChange}></input>
-    </span>
+    <div>
+      <h3>Seed: <input type="text" onChange={handleChange}></input></h3>
+      <h3>The Hitchin's traits:</h3>
+      <HitchinTraitsDump seed={text}></HitchinTraitsDump>
+    </div>
+  )
+}
+
+function HitchinTraitsDump({seed}: {seed: string}) {
+  
+  return (
+    <table>
+      <tr></tr>
+    </table>
+  )
+}
+
+function TraitRow({name, stringValue}: {name: string, stringValue: string}) {
+  return (
+    <tr>
+      <td>{name}:</td>
+      <td>{stringValue}</td>
+    </tr>
   )
 }
 
@@ -18,7 +42,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <SeedField></SeedField>
+        <SeedTester></SeedTester>
       </main>
     </div>
   );
