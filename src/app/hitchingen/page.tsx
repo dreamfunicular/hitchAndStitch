3dec32196deec84313ba7f10ea3b3e4704a1a1bd
UnrelatedString <unrelatedstring@gmail.com>
2025-02-15 15:48:05 -0500

    WHAT IS A CLIENT COMPONENT ;_;

--- src/app/hitchingen/page.tsx
+++ src/app/hitchingen/page.tsx
@@ -1,10 +1,20 @@
+'use client';
+
 import Image from "next/image";
+
+function SeedField() {
+  return (
+    <span>
+      Seed: <input type="text" onChange={_=>alert("aaaaa")}></input>
+    </span>
+  )
+}
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Seed: <input type="text" onChange={_=>alert("aaaaa")}></input>
+        <SeedField></SeedField>
       </main>
     </div>
   );
