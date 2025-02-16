86f2942928597435b4aeb63fe52f51b45483bf23
Anna Lynch <anna@fruitofthefirehose.org>
2025-02-16 00:06:54 -0500

    AUTH WORKS (cookie retention unbuilt--forge path soon)

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,4 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
+import { Pardner } from "@/app/lib/definitions";
+import "@/app/lib/data";
+import { fetchPardner } from "@/app/lib/data";
+import { cookies } from "next/headers";
 
 interface Inc {
   username: string;
@@ -6,10 +10,37 @@ interface Inc {
 }
 
 export async function POST(req: NextRequest) {
+  try {
+    const cookieStore = await cookies();
+    const inAuth = cookieStore.get("authToken");
+    console.log("In auth:" + inAuth?.value);
+  } catch (e) {
+    console.log("No auth cookie.");
+  }
+
   const data: Inc = await req.json();
 
-  console.log(data.username);
-  console.log(data.password);
+  let submittedUsername: string = data.username;
+  let submittedPassword: string = data.password;
 
-  return NextResponse.json({ status: 200 });
+  let p = await fetchPardner();
+  let matchFound = false;
+
+  for (var i = 0; i < p.length; i++) {
+    if (p[i].username == submittedUsername) {
+      if (p[i].password == submittedPassword) {
+        matchFound = true;
+      }
+
+      break;
+    }
+  }
+
+  if (matchFound) {
+    let authToken = crypto.randomUUID();
+    console.log(authToken);
+    return NextResponse.json({ status: 200, authToken: authToken });
+  } else {
+    return NextResponse.json({ status: 403 });
+  }
 }
--- src/app/ui/loginForm.tsx
+++ src/app/ui/loginForm.tsx
@@ -3,15 +3,22 @@
 // if wronggoing, blame anna!
 
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { FormEvent } from "react";
 import { useState } from "react";
+import { redirect } from "next/navigation";
 
 export default function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [warningText, setWarningText] = useState("");
 
   async function submit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    if (username == "" || password == "") {
+      setWarningText("Please submit a username and password.");
+    }
 
     const res = await fetch("/api/login", {
       method: "POST",
@@ -26,7 +33,15 @@ export default function LoginForm() {
 
     const data = await res.json();
 
-    console.log(data.status);
+    if (data.status == 200) {
+      console.log("Worked!");
+      document.cookie = "authToken=" + data.authToken;
+
+      redirect("/");
+    } else {
+      console.log("didn't work :(");
+      setWarningText("That username or password isn't right.");
+    }
   }
 
   return (
@@ -49,6 +64,7 @@ export default function LoginForm() {
         onChange={(e) => setPassword(e.target.value)}
         className="bg-white h-10 w-80 rounded-xl m-2 text-gray-800 indent-2 placeholder-gray-400"
       />
+      <p className="text-2"> {warningText} </p>
       <button
         type="submit"
         className="bg-orange-200 text-gray-400 rounded-xl w-fit p-1 m-2 hover:bg-orange-300 hover:text-gray-600 border-gray-400 border-1">
