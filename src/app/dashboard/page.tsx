89f98d089233dbc7d5cc73c42554fbc17cfff57c
shinadoll <yamanakamay39@protonmail.com>
2025-02-16 10:05:25 -0500

    qr code in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { cookies } from "next/headers";
 import { Pardner, Hitchin, Relationship } from "@/app/lib/definitions";
 import { redirect } from "next/navigation";
 import { fetchPardner } from "@/app/lib/data";
+import QRCode from "react-qr-code";
 
 var activePardner: Pardner;
 
@@ -31,7 +32,7 @@ export default async function Dashboard() {
 
   return (
     <div className="flex flex-col w-screen h-screen bg-orange-200 items-center justify-center">
-      {" "}
+      <QRCode value={"http://172.20.188.222:3000/transferHitchin?pardner-id=" + activePardner.id + "&hitchin-id=" + 2/* put new hitchin here*/}/>
     </div>
   );
 }
--- src/app/transferHitchin/page.tsx
+++ src/app/transferHitchin/page.tsx
@@ -7,12 +7,14 @@ export default function Main () {
     const userid = 1; //add code for user cookies
 
     const queryParameters = new URLSearchParams(window.location.search);
-    let id : number;
-    id = queryParameters.get("id") as unknown as number;
+    let newPardnerID : number;
+    newPardnerID = queryParameters.get("pardner-id") as unknown as number;
+    let newHitchinID : number;
+    newHitchinID = queryParameters.get("hitchin-id") as unknown as number;
     const newRelationship : Relationship = {
         id : 0,
         active : true,
-        hitchinID: id,
-        pardnerID: userid,};
+        hitchinID: newHitchinID,
+        pardnerID: newPardnerID,};
     createRelationship(newRelationship)
 }
